f5c83c6971cfb5fc6cd23c2a4e4388fcd2104adb
jacksonneves <jackson.neves@senior.com.br>
2020-12-09 21:42:55 -0300

    added animations in search bar

--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,13 +1,29 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { RestaurantsService } from './restaurants.service';
 import { Restaurant } from './restaurant-list/restaurant-list.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
 
+  searchBarState = 'hidden';
   restaurants: Restaurant[];
 
   constructor(private restaurantsService: RestaurantsService) { }
@@ -18,4 +34,7 @@ export class RestaurantComponent implements OnInit {
       .subscribe(restaurants => this.restaurants = restaurants);
   }
 
+  toggleSearch() {
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
+  }
 }
